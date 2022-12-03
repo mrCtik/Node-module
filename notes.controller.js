b89b3accdb3a8f12a1764dacc320e66e67a6645e
mrCtik <99313450+mrCtik@users.noreply.github.com>
2022-12-04 03:01:11 +0700

    task-3 complited

--- notes.controller.js
+++ notes.controller.js
@@ -5,45 +5,54 @@ const chalk = require('chalk')
 const notesPath = path.join(__dirname, 'db.json')
 
 async function addNote(title) {
-  const notes = await getNotes()
-  const note = {
-    title,
-    id: Date.now().toString()
-  }
+    const notes = await getNotes()
+    const note = {
+        title,
+        id: Date.now().toString()
+    }
 
-  notes.push(note)
+    notes.push(note)
 
-  await saveNotes(notes)
-  console.log(chalk.bgGreen('Note was added!'))
-}
-
-async function removeNotes(id){
-  const notesString = await fs.readFile(notesPath, {encoding: 'utf-8'})
-  const notes = JSON.parse(notesString)
-  let filtredNotes = notes.filter(note => note.id !== id)
-  await fs.writeFile(notesPath, JSON.stringify(filtredNotes))
-  console.log(chalk.bgRed(`Note ${id} was removed!`))
-
+    await saveNotes(notes)
+    console.log(chalk.bgGreen('Note was added!'))
 }
 
 async function getNotes() {
-  const notes = await fs.readFile(notesPath, {encoding: 'utf-8'})
-  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : []
+    const notes = await fs.readFile(notesPath, {encoding: 'utf-8'})
+    return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : []
 }
 
 async function saveNotes(notes) {
-  await fs.writeFile(notesPath, JSON.stringify(notes))
+    await fs.writeFile(notesPath, JSON.stringify(notes))
 }
 
 async function printNotes() {
-  const notes = await getNotes()
+    const notes = await getNotes()
 
-  console.log(chalk.bgBlue('Here is the list of notes:'))
-  notes.forEach(note => {
-    console.log(chalk.bgWhite(note.id), chalk.blue(note.title))
-  })
+    console.log(chalk.bgBlue('Here is the list of notes:'))
+    notes.forEach(note => {
+        console.log(chalk.bgWhite(note.id), chalk.blue(note.title))
+    })
+}
+
+async function removeNote(id) {
+    const notes = await getNotes()
+    const filtered = notes.filter(note => note.id !== id)
+
+    await saveNotes(filtered)
+    console.log(chalk.red(`Note with id="${ id }" has been removed.`))
+}
+
+async function editNotes(id, title) {
+    const notes = await getNotes()
+    const netNotes = notes.map(note => {
+        if (note.id === id) note.title = title
+        return note
+    })
+    await saveNotes(netNotes)
+    console.log(chalk.yellow(`Note with id="${ id }" has been edited.`))
 }
 
 module.exports = {
-  addNote, printNotes, removeNotes
+    addNote, getNotes, removeNote, editNotes
 }
